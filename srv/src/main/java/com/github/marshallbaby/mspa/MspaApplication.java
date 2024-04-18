75079434eb4a55690198ab4840bdf87f5cadd052
Alexey Yatsevich <aleksey.yatsevich@gmail.com>
2024-04-18 20:02:02 +0200

    Setup initial mta.yaml configuration. Setup initial Spring Boot application.

---
+++ srv/src/main/java/com/github/marshallbaby/mspa/MspaApplication.java
@@ -0,0 +1,13 @@
+package com.github.marshallbaby.mspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MspaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MspaApplication.class, args);
+	}
+
+}
---
+++ srv/src/test/java/com/github/marshallbaby/mspa/MspaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marshallbaby.mspa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MspaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
