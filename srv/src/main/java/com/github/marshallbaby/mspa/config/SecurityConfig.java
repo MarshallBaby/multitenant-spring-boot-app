171e6a2d121d5eee6fc4de4dadf23968fb6c7d6d
Alexey Yatsevich <aleksey.yatsevich@gmail.com>
2024-04-23 15:37:16 +0200

    Setup local profile for local development.

--- srv/src/main/java/com/github/marshallbaby/mspa/config/SecurityConfig.java
+++ srv/src/main/java/com/github/marshallbaby/mspa/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.sap.cloud.security.xsuaa.token.TokenAuthenticationConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -20,14 +21,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
-
-    // TODO: Setup security for local environment
-    // TODO: Setup/try use of roles
-
     @Autowired
     XsuaaServiceConfiguration xsuaaServiceConfiguration;
 
     @Bean
+    @Profile("cloud")
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(authz ->
@@ -38,6 +36,17 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwtConfigurer ->
                                 jwtConfigurer.jwtAuthenticationConverter(getJwtConverter())));
+
+        return http.build();
+    }
+
+    @Bean
+    @Profile("local")
+    public SecurityFilterChain filterChainLocal(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(authz ->
+                authz.anyRequest().permitAll()
+        );
 
         return http.build();
     }
